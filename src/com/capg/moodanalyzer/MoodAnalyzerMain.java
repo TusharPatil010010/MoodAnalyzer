410dfcf283eaf8eaa02ef3021710c466df129f11
TusharPatil010010 <tusharp010010@gmail.com>
2020-10-05 22:50:06 +0530

    Verified second test case with empty input

--- src/com/capg/moodanalyzer/MoodAnalyzerMain.java
+++ src/com/capg/moodanalyzer/MoodAnalyzerMain.java
@@ -6,7 +6,7 @@ public class MoodAnalyzerMain {
 			
 			//Creating an object of Mood Analyzer
 			try {
-				MoodAnalyzer MoodAnalyzerObject = new MoodAnalyzer(); 
+				MoodAnalyzer MoodAnalyzerObject = new MoodAnalyzer(""); //Empty Mood Test Case
 				MoodAnalyzerObject.analyzeMood();
 			} catch (MoodAnalysisException e) {
 				e.printStackTrace();
